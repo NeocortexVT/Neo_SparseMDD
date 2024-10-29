60f48a9bb782bfef8bd39f8f31cbb26efbf5725c
NeocortexVT <neocortexvt@proton.me>
2024-10-29 18:34:59 +0100

    Add files via upload

---
+++ InputManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Neo_SparseMMD
+{
+    public class InputManager : MonoBehaviour
+    {
+        public InputField A;
+        public InputField B;
+        public InputField C;
+        public InputField D;
+        public InputField E;
+        //public InputField F;
+    }
+}
